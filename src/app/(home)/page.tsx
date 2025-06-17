b13df68b9811a43bb6f8a361dc76e04051209246
thanh trung <ntrung1121@gmail.com>
2025-06-17 16:15:46 +0700

    adding trpc init

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
-	return <div>main section here</div>;
+import { trpc } from "@/trpc/server";
+
+export default async function Home() {
+	const data = await trpc.hello({ text: "Antonio" });
+	return <div>main section here with data: {data.greeting}</div>;
 }
